6f6f195cde2daf85b956a247f460028318e9b622
Roy <levanomoermond21@gmail.com>
2025-12-03 17:14:46 +0100

    Configure SQLAlchemy and initialize database connection

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+from models import User
+
 @app.route('/')
 def home():
     return render_template("index.html")
