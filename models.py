fa2876d467d87db4db0b0e4aa9b94fbe06842ad7
Roy <levanomoermond21@gmail.com>
2025-12-03 17:59:47 +0100

    working test crud page for debugging

--- models.py
+++ models.py
@@ -10,3 +10,11 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Item {self.name}>"
